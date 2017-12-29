236b4b5e4425caf5c16b610f1ca0e678f66c14d4
Neil Munro <neil.munro@wearefarm.com>
2017-12-29 20:31:30 +0000

    Added basic proverbs view.

--- proverbs/views.py
+++ proverbs/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Proverb
 
 def index(request):
-    return render(request, 'proverbs/index.html')
+    proverbs = list(Proverb.objects.all())
+    return render(request, 'proverbs/index.html', {'proverbs': proverbs})
