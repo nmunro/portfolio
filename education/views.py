b6cd90e9ef596dc5823c6179dab52f210f96b129
Neil Munro <neil.munro@wearefarm.com>
2017-12-29 19:40:38 +0000

    Display educational data.

--- education/views.py
+++ education/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Education
+
 
 def index(request):
-    return render(request, 'education/index.html')
+    l = list(Education.objects.all().order_by('-completed'))
+    return render(request, 'education/index.html', {"education": l})
