6c6dd54d2ba2c4f8821e53c4712d530fb917a4d3
Neil Munro <neil.munro@wearefarm.com>
2017-12-30 23:57:07 +0000

    Added settings for base, dev and prod
    Added settings for base, dev and prod.

---
+++ portfolio/settings/prod.py
@@ -0,0 +1,10 @@
+from .base import *
+
+DATABASES = {
+    'default': {
+        'NAME': 'portfolio',
+        'ENGINE': 'django.db.backends.postgresql',
+        'USER': 'neil',
+        'PASSWORD': 'AC1314127c2!'
+    }
+}
