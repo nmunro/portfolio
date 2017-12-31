8a9d3fe292f5f9cb55d5973df9e3f85e90e174d4
Neil Munro <neil.munro@wearefarm.com>
2017-12-31 12:16:11 +0000

    Fixed dark theme
    Fixed dark theme.

--- portfolio/settings/base.py
+++ portfolio/settings/base.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'l)!ibhd75p@6kk5u#^ilmhx7fx%%5$5&_(8a)4lx*3%9dx4r)9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -112,3 +113,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+STATICFILES_FINDERS = [
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    'django.contrib.staticfiles.finders.AppDirectoriesFinder',
+]
+
+PROJECTDIR = os.path.dirname(os.path.realpath(__file__))[:-9]
+STATICFILES_DIRS = (PROJECTDIR + '/static',)
